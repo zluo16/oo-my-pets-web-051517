047062ef75c2edd0dce625d0fda310b999baeffc
zluo16 <zluo@alumni.berklee.edu>
2017-05-29 16:54:28 -0400

    practice pets 1

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,31 +1,40 @@
+require "pry"
+require_relative "cat.rb"
+require_relative "dog.rb"
+require_relative "fish.rb"
+
 class Owner
 
-  attr_accessor :name, :pets
-  attr_reader :species
+  attr_accessor :pets
+  attr_reader :name, :species
 
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
     @species = "human"
-    @pets = {:fishes => [], :dogs => [], :cats => []}
-  end
-
-  def self.reset_all
-    @@all.clear
-  end
-
-  def self.count
-    @@all.length
+    @pets = {:cats => [], :dogs => [], :fishes => []}
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def self.reset_all
+    @@all = Array.new
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def name
+    @name
+  end
+
   def say_species
-    "I am a human."
+    "I'm a human."
   end
 
   def buy_fish(name)
@@ -40,12 +49,12 @@ class Owner
     @pets[:dogs] << Dog.new(name)
   end
 
-  def walk_dogs
-    @pets[:dogs].each{|dog| dog.mood = "happy"}
-  end
-
   def play_with_cats
     @pets[:cats].each{|cat| cat.mood = "happy"}
+  end
+
+  def walk_dogs
+    @pets[:dogs].each{|dog| dog.mood = "happy"}
   end
 
   def feed_fish
@@ -53,12 +62,33 @@ class Owner
   end
 
   def sell_pets
-    @pets.each{|species, arr| arr.each{|pet| pet.mood = "nervous"}}
-    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @pets.each do |spec, pets|
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+      pets.clear
+    end
   end
 
   def list_pets
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    "I have #{pets[:fishes].length} fish, #{pets[:cats].length} cat(s), and #{pets[:dogs].length} dog(s)."
   end
 
 end
+
+owner = Owner.new("Kevin")
+puts owner.name
+puts owner.buy_cat("Crookshanks")
+puts owner.buy_fish("Nemo")
+puts owner.buy_dog("Fido")
+
+owner.play_with_cats
+puts owner.pets[:cats].first.mood
+owner.walk_dogs
+puts owner.pets[:dogs].first.mood
+owner.feed_fish
+puts owner.pets[:fishes].first.mood
+
+puts owner.sell_pets
+puts Owner.all
+puts Owner.count
